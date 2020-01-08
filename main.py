df63d3c6277d05e89d6cd9d17de12197057ac7c4
liao <liao961120@gmail.com>
2020-01-08 14:15:54 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+import re
 import os
 from bs4 import BeautifulSoup
+
 
 def createItem(lemma: str, page: str, zh_def: str, ori_def: str, item_id=None, lemma_id=None):
     soup = BeautifulSoup(features="html.parser")
@@ -22,7 +24,7 @@ def createItem(lemma: str, page: str, zh_def: str, ori_def: str, item_id=None, l
     lemma_tag.string = lemma
     page_tag.string = page
     zh_def_tag.string = zh_def
-    ori_def_tag.string = ori_def
+    ori_def_tag.append(BeautifulSoup(ori_def, "html.parser"))
     if item_id:
         item_tag['id'] = str(item_id)
     if lemma_id:
@@ -30,7 +32,47 @@ def createItem(lemma: str, page: str, zh_def: str, ori_def: str, item_id=None, l
 
     return str(item_tag)
 
+def import_from_gsheet():
+    gids = ['679511153', '1931724449', '151044886', '1128079217', '1431072574', '866770763', '1207822401', '396391367', '1345694829', '939992016', '971568665', '1842077571']
+    pages = ['妤蓁_13-41', '曉鈞_42-70', '容榕_71-99', '永賦_100-128', '庭瑋_129-157', '飛揚_158-186', '俊宏_187-215', '瑞恩_216-244', '凱弘_247-273', '晴方_274-302', '莊勻_303-331', '峻維_326-360']
+    url = "https://docs.google.com/spreadsheets/d/186qohB4p9_ewDqggE547E92bxYTILKuSm26PWljVInk/export?format=csv&gid={gid}"
+    dfs = []
+    for gid, sheet in zip(gids, pages):
+        df = pd.read_csv(url.format(gid=gid), dtype='str').dropna(subset=["詞條", "釋義"]).replace(np.nan, '')
+        df['轉寫者'] = sheet
+        dfs.append(df)
+    merged_df = pd.concat(dfs, sort=False, ignore_index=True)
+    merged_df.to_csv("favorlang_dict.csv", index=False)
 
+    return merged_df
+
+
+def get_all_lemma(df, lemma_col='詞條'):
+    all_lemma = set(df[lemma_col].values)
+    all_lemma2 = list(all_lemma)
+    for l in list(all_lemma):
+        if '*' in l:
+            all_lemma2.remove(l)
+    return all_lemma2
+
+
+def index_lemma_in_def(df, all_lemma, def_col='釋義'):
+    indexed_defs = []
+    pat = " (" + '|'.join(all_lemma) + ")[\.,;]? "
+    for def_ in df[def_col].values:
+        # Wrap <a> tag around lemma in definition
+        replacement = ' <a class="idx" href="#">\g<1></a> '
+        def_ = re.sub(pat, replacement, def_)
+        
+        # Add href in <a> tag
+        tag = BeautifulSoup(def_, "html.parser")
+        for a in tag.find_all('a', attrs={'class':'idx'}):
+            a["href"] = '#' + a.text.lower() + '_1'
+        def_ = str(tag)
+        indexed_defs.append(def_)
+    return indexed_defs
+
+########------------------------ Main Function --------------------##########
 
 def main():
     #------------------ Config ------------------#
@@ -67,18 +109,11 @@ def main():
     </html>
     """
 
-
     #--------------------- Get data from google sheets -------------------#
-    gids = ['679511153', '1931724449', '151044886', '1128079217', '1431072574', '866770763', '1207822401', '396391367', '1345694829', '939992016', '971568665', '1842077571']
-    pages = ['妤蓁_13-41', '曉鈞_42-70', '容榕_71-99', '永賦_100-128', '庭瑋_129-157', '飛揚_158-186', '俊宏_187-215', '瑞恩_216-244', '凱弘_247-273', '晴方_274-302', '莊勻_303-331', '峻維_326-360']
-    url = "https://docs.google.com/spreadsheets/d/186qohB4p9_ewDqggE547E92bxYTILKuSm26PWljVInk/export?format=csv&gid={gid}"
-    dfs = []
-    for gid, sheet in zip(gids, pages):
-        df = pd.read_csv(url.format(gid=gid), dtype='str').dropna(subset=["詞條", "釋義"]).replace(np.nan, '')
-        df['轉寫者'] = sheet
-        dfs.append(df)
-    merged_df = pd.concat(dfs, sort=False, ignore_index=True)
-    merged_df.to_csv("favorlang_dict.csv", index=False)
+    merged_df = import_from_gsheet()
+    #---------------- Index lemma mentioned in def ----------------#
+    all_lemma = get_all_lemma(merged_df)
+    merged_df['釋義'] = index_lemma_in_def(merged_df, all_lemma)
 
 
     #---------------- Convert pd to HTML file ---------------------#
